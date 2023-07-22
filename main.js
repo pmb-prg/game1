ac35bbd993af3973f3958ee5eff3384bc2ba4f74
PMBPG <pmbpk@yahoo.com>
2023-07-22 18:17:15 +0330

    Completed this project

--- main.js
+++ main.js
@@ -5,65 +5,92 @@ const scissors = document.querySelector(".scissors");
 /*--------------player--------------------*/
 const bot = document.querySelector(".bot");
 const you = document.querySelector(".you");
+/*-----------------------------------------*/
+const massage = document.querySelector(".massage");
+const textMass = document.querySelector(".text-mass");
+const btnMass = document.querySelector(".btn-mass");
+
+
+btnMass.addEventListener("click", () => {
+    massage.style.display = "none";
+})
 
 
 
-const botChose = ["image/rock2.png", "image/paper2.png", "image/scissors2.png"];
-let random = "";
-
-
-
-
-function Condition() {
-    if (you.src === "image/rock.png") {
-        console.log("work");
-
-    }
+function randomSelect() {
+    const botChose = ["image/rock2.png", "image/paper2.png", "image/scissors2.png"];
+    let random = "";
+    random = botChose[Math.floor(Math.random() * botChose.length)];
+    bot.src = `${random}`;
+    bot.style.animation = "none";
+    you.style.animation = "none"
     return;
 }
-Condition();
+
+function animation() {
+    you.src = "image/rock.png";
+    bot.src = "image/rock2.png";
+    bot.style.animation = "shake 3s infinite";
+    you.style.animation = "shake2 3s infinite";
+    return;
+}
+
+
+
+
+
+function condition(b, y) {
+    if ((b === "http://127.0.0.1:5500/image/rock2.png" && y === "http://127.0.0.1:5500/image/rock.png") ||
+        (b === "http://127.0.0.1:5500/image/paper2.png" && y === "http://127.0.0.1:5500/image/paper.png") ||
+        (b === "http://127.0.0.1:5500/image/scissors2.png" && y === "http://127.0.0.1:5500/image/scissors.png")) {
+        massage.style.display = "flex";
+        textMass.innerText = "Equal";
+        textMass.style.color = "black";
+    }
+    if ((b === "http://127.0.0.1:5500/image/rock2.png" && y === "http://127.0.0.1:5500/image/paper.png") ||
+        (b === "http://127.0.0.1:5500/image/paper2.png" && y === "http://127.0.0.1:5500/image/scissors.png") ||
+        (b === "http://127.0.0.1:5500/image/scissors2.png" && y === "http://127.0.0.1:5500/image/rock.png")) {
+        massage.style.display = "flex";
+        textMass.innerText = "You Win";
+        textMass.style.color = "green";
+    }
+    if ((b === "http://127.0.0.1:5500/image/rock2.png" && y === "http://127.0.0.1:5500/image/scissors.png") ||
+        (b === "http://127.0.0.1:5500/image/paper2.png" && y === "http://127.0.0.1:5500/image/rock.png") ||
+        (b === "http://127.0.0.1:5500/image/scissors2.png" && y === "http://127.0.0.1:5500/image/paper.png")) {
+        massage.style.display = "flex";
+        textMass.innerText = "You Lose";
+        textMass.style.color = "red";
+    }
+
+
+
+
+}
+
 rock.addEventListener("click", function() {
-    you.src = "image/rock.png";
-    bot.src = "image/rock2.png";
-    bot.style.animation = "shake 3s infinite";
-    you.style.animation = "shake2 3s infinite";
-
+    animation();
     setTimeout(() => {
-        random = botChose[Math.floor(Math.random() * botChose.length)];
-        bot.src = `${random}`;
-        bot.style.animation = "none";
-        you.style.animation = "none"
+        randomSelect();
         you.src = "image/rock.png";
+        condition(bot.src, you.src);
     }, 3600);
 
-})
-
+});
 paper.addEventListener("click", function() {
-    you.src = "image/rock.png";
-    bot.src = "image/rock2.png";
-    bot.style.animation = "shake 3s infinite";
-    you.style.animation = "shake2 3s infinite";
-
+    animation();
     setTimeout(() => {
-        random = botChose[Math.floor(Math.random() * botChose.length)];
-        bot.src = `${random}`;
-        bot.style.animation = "none";
-        you.style.animation = "none"
+        randomSelect();
         you.src = "image/paper.png";
+        condition(bot.src, you.src);
     }, 3600);
-})
 
+});
 scissors.addEventListener("click", function() {
-    you.src = "image/rock.png";
-    bot.src = "image/rock2.png";
-    bot.style.animation = "shake 3s infinite";
-    you.style.animation = "shake2 3s infinite";
-
+    animation();
     setTimeout(() => {
-        random = botChose[Math.floor(Math.random() * botChose.length)];
-        bot.src = `${random}`;
-        bot.style.animation = "none";
-        you.style.animation = "none"
+        randomSelect();
         you.src = "image/scissors.png";
+        condition(bot.src, you.src);
     }, 3600);
-})
+
+});
